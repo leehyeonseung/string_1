e56e709daca9ae4cb39c0c3d007816be27500e4a
이현승 <@DESKTOP-6R4PVRU>
2020-02-22 13:00:49 +0900

    1.초난감 DAO 만들기 및 리팩토링

--- src/main/java/com_study/stringStudy_1/App.java
+++ src/main/java/com_study/stringStudy_1/App.java
@@ -1,13 +1,27 @@
 package com_study.stringStudy_1;
 
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws ClassNotFoundException, SQLException
     {
-        System.out.println( "Hello World!" );
+       UserDao dao= new UserDao();
+       
+       User user= new User();
+       user.setId("던킨");
+       user.setName("도넛");
+       user.setPassword("8");
+       
+       dao.add(user);
+       
+       System.out.println(user.getId()+"등록 성공");
+       
+       User user2 = dao.get(user.getId());
+       System.out.println(user2.getName());
     }
 }
