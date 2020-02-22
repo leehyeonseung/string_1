f44cc4a9b1c34858daea961bf1675599f2b9df28
이현승 <@DESKTOP-6R4PVRU>
2020-02-22 15:46:15 +0900

    4-1 제어의역전 ( 어노테이션방법으로 ) 빈 생성하기

--- src/main/java/com_study/stringStudy_1/App.java
+++ src/main/java/com_study/stringStudy_1/App.java
@@ -2,23 +2,28 @@ package com_study.stringStudy_1;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args ) throws ClassNotFoundException, SQLException
     {
     	//User DAO = 추상 클래스 이므로 객체를 직접 생성할 수 없다
     	// 그러므로 상속한 클래스를 주입해주어야 한다 ( user dao 에 class 앞에 abstract 가 있을 경우 에러)
-		 UserDao dao= new DaoFactory().userDao(); 
+		/* UserDao dao= new DaoFactory().userDao(); */
     
-      
+      ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+      UserDao dao = context.getBean("userDao",UserDao.class);
        
        User user= new User();
-       user.setId("던킨2232");
-       user.setName("도넛2322");
+       user.setId("치킨");
+       user.setName("피자");
        user.setPassword("8");
        
        dao.add(user);
--- src/main/java/com_study/stringStudy_1/DaoFactory.java
+++ src/main/java/com_study/stringStudy_1/DaoFactory.java
@@ -1,9 +1,22 @@
 package com_study.stringStudy_1;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
 
+	@Bean
 	public UserDao userDao() {
-		UserDao userDao = new UserDao(new DConnectionMaker());
-		return userDao;
+		/* UserDao userDao = new UserDao(new DConnectionMaker()); */
+		return new UserDao(connectionMaker());
 	}
+	
+	@Bean
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
+	}
+	
+	
+	
 }
